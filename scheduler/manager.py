337aaab2f0f099527e5a716788b6f878d274f4d5
Mykola Topikha <goldmasterua@gmail.com>
2021-03-10 20:58:28 +0200

    Menu, Database, Scheduler
    Menu — add class SimpleMenu to manage Database: adding new jobs
    Database —  method to get  last unused id, change database stucture
    Scheduler — clean up, new method to open url

--- scheduler/manager.py
+++ scheduler/manager.py
@@ -4,6 +4,7 @@ from scheduler.database import SchedulerDatabase
 from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime, timedelta
 from logging import getLogger, WARNING
+from os import startfile
 from json import load
 
 
@@ -13,11 +14,10 @@ class CustomScheduler():
         self.database = database
         self.scheduler = BlockingScheduler()
         self.logger = getLogger(__name__)
-
-    def disable_apscheduler_logger(self):
-        """ Optional """
-        getLogger('apscheduler.scheduler').setLevel(WARNING)
-        getLogger('apscheduler.executors.default').setLevel(WARNING)
+        self.actions = [
+            ['send_and_delete_message', self.send_and_delete_message],
+            ['open_link_with_delay', self.open_link_with_delay]
+        ]
 
     def load_settings(self, filename: str):
         with open(file=filename, mode='r', encoding='utf-8') as settings_file:
@@ -25,22 +25,67 @@ class CustomScheduler():
 
         self.default_group_id: int = settings.get('test_group_id')
 
-    def send_and_delete_message(self, payload: dict):
+    def reload_database(self):
+        self.scheduler.remove_all_jobs()
+        self.database.load_database()
+        self.create_jobs()
+        self.database.load_database()
+
+    def disable_apscheduler_logger(self):
+        """ Optional """
+        getLogger('apscheduler.scheduler').setLevel(WARNING)
+        getLogger('apscheduler.executors.default').setLevel(WARNING)
+
+    def add_delay_job_run_once(self, func, delta, kwargs):
+        run_date = datetime.now() + timedelta(**delta)
+        self.scheduler.add_job(func=func, trigger="date",
+                               run_date=run_date, kwargs=kwargs)
+
+    def send_message(self, payload):
         payload.get('message').update(
             text='\n'.join(payload.get('message').get('text')))
-        message_id = self.bot.send_message(
-            chat_id=self.default_group_id, **payload.pop('message'))
+        return self.bot.send_message(chat_id=self.default_group_id, **payload.pop('message'))
+
+    def delete_message(self, payload, message_id):
         kwargs = {
-            'chat_id': self.default_group_id,
-            'message_id': message_id
+            'func': self.bot.delete_message,
+            'delta': payload.pop('timedelta'),
+            "kwargs": {
+                'chat_id': self.default_group_id,
+                'message_id': message_id
+            }
         }
-        run_date = datetime.now() + timedelta(**payload.pop('timedelta'))
-        self.scheduler.add_job(self.bot.delete_message,
-                               trigger="date", run_date=run_date, kwargs=kwargs)
+        self.add_delay_job_run_once(**kwargs)
+
+    def send_and_delete_message(self, payload: dict):
+        message_id = self.send_message(payload)
+        self.delete_message(payload, message_id)
+
+    def open_zoom_link(self, payload: dict):
+        # TODO:
+        #  parse link and open through build in app
+        #  not though browser it's annoying
+        kwargs = {
+            'func': startfile,
+            'delta': payload.pop('timedelta'),
+            'kwargs': {
+                'filepath': payload.pop('url')
+            }
+        }
+        self.add_delay_job_run_once(**kwargs)
+
+    def open_link_with_delay(self, payload: dict):
+        url = payload.get('url')
+        if 'zoom.us' in url:
+            self.open_zoom_link(payload)
 
     def timeout_job_manager(self, payload: dict):
-        if payload.pop('action') == 'send_and_delete_message':
-            self.send_and_delete_message(payload)
+        for income_action in payload:
+            action = income_action.pop('action')
+
+            for action_name, func in self.actions:
+                if action == action_name:
+                    func(income_action)
 
     def create_job(self, job: dict):
         self.scheduler.add_job(func=self.timeout_job_manager,
@@ -48,6 +93,7 @@ class CustomScheduler():
 
     def create_jobs(self):
         for job in self.database.get_all_jobs():
+            self.logger.info(f'Processing job with id: {job.pop("id")}')
             self.create_job(job)
 
     def start(self):
